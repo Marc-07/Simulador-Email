647fca0e49c05e093d6ad57ddf00f8a2ff084919
Maria Ramos <maanraca01@gmail.com>
2024-08-30 15:16:24 -0600

    Se crea un evento para resetaer el formulario

--- src/app.js
+++ src/app.js
@@ -14,13 +14,28 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputAsunto = document.querySelector("#asunto");
     const inputMensaje = document.querySelector("#mensaje");
     const formulario = document.querySelector("#formulario");
-    const btnSubmit = document.querySelector('#formulario button[type="submit"]')
+    const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]')
 
     //Asignar eventos
     inputEmail.addEventListener("input", validar);
     inputAsunto.addEventListener("input", validar);
     inputMensaje.addEventListener("input", validar);
     inputNombre.addEventListener("input", validar)
+
+    btnReset.addEventListener("click", function (e){
+        e.preventDefault();
+
+        //Reiniciar el objeto
+        email.nombre = "";
+        email.email = "";
+        email.asunto = "";
+        email.mensaje = "";
+
+        formulario.reset();
+        comprobarEmail();
+
+    })
 
     function validar (e){
 
